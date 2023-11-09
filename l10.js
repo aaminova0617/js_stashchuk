f321a0be1374ca14ca81db2bd908a0411808f681
aziza aminova <aziza@acompleteappliance.com>
2023-11-09 11:31:07 -0500

    10 ssyloch

---
+++ l10.js
@@ -0,0 +1,21 @@
+/*
+
+const a1 = {
+    aaa: 12,
+    bbb: 'wer',
+}
+
+console.log('a1 is ',a1)
+
+const b = a1
+
+b.aaa = 'stroka'
+b.bbb = 23
+
+console.log('a1 is ',a1)
+*/
+
+const objectA = {
+    a:10,
+    b:true
+}
