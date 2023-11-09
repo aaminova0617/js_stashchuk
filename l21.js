94203b41145ee0d8f00685efe1c198489a208a7f
aziza aminova <aziza@acompleteappliance.com>
2023-11-09 17:47:51 -0500

    functions

---
+++ l21.js
@@ -0,0 +1,39 @@
+/*
+let a = 5
+let b = 3
+let c
+
+c=a+b
+console.log(c)
+
+a = 8
+b = 12
+
+c=a+b
+console.log(c)
+
+*/
+
+let a = 5
+let b = 3
+
+function sum(a, b) {
+    const c=a+b
+    console.log(c)
+}
+
+sum(a,b)
+a = 8
+b = 12
+sum(a,b)
+sum(1,2)
+
+console.dir(sum)
+
+blaFn = function (a, b) {
+    const c=a+b
+    console.log(c)
+}
+
+blaFn(a,b)
+console.dir(blaFn)
