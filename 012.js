3de7e9766e68308dfb051d3b679a077238880e5a
aziza aminova <aziza@acompleteappliance.com>
2023-11-09 12:30:16 -0500

    12object json

---
+++ 012.js
@@ -0,0 +1,12 @@
+const myCity = {
+    city: 'New York',
+    popular: true,
+    country:'USA'
+}
+console.log(myCity)
+console.log(myCity.city)
+console.log(myCity.popular)
+
+myCity.city = 'las vegas'
+
+console.log(myCity)
