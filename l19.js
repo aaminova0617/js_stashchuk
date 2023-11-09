7e80e7f5c8413f104a519350d91c3875452a8f2f
aziza aminova <aziza@acompleteappliance.com>
2023-11-09 15:42:07 -0500

    19mutant

---
+++ l19.js
@@ -0,0 +1,18 @@
+const a= 10
+let b= a
+b=30
+console.log(a)
+console.log(b)
+
+console.log('----------------------------------')
+
+const person = {
+    name: 'Bob',
+    age: 21
+}
+
+person.age = 22
+person.isAdult = true
+
+console.log(person.age)
+
