d09660870736fee1ea8c2dba1107042616a404cd
aziza aminova <aziza@acompleteappliance.com>
2023-11-10 14:51:45 -0500

    38 type of practice

---
+++ l37.js
@@ -0,0 +1,25 @@
+in web console
+
+
+typeof 10
+'number'
+typeof undefined
+'undefined'
+typeof "aziza"
+'string'
+typeof false
+'boolean'
+type of 10==='number'
+VM387:1 Uncaught SyntaxError: Unexpected identifier 'of'
+typeof10==='number'
+VM427:1 Uncaught ReferenceError: typeof10 is not defined
+at <anonymous>:1:1
+(anonymous) @ VM427:1
+typeof 10 === 'number'
+true
+let isUndefined
+undefined
+typeof isUn
+'undefined'
+typeof isUndefined === 'undefined'
+true
