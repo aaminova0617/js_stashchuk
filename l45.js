d447f04a7ec149d50fe3b44af99b9659295cf32a
aziza aminova <aziza@acompleteappliance.com>
2023-11-10 15:50:46 -0500

    45konkatenacia

---
+++ l45.js
@@ -0,0 +1,8 @@
+ const name = 'aziza'
+ const city = 'cleveland'
+
+ const greeting = `my name is ${name} I live in ${city}`
+ console.log(greeting)
+
+const greeting1 = 'my name is ' + name + ' I live in ' + city
+ console.log(greeting1)
