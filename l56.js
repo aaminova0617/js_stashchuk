20c853ca3ba6624412d1ac0d681ddf1bbdeb626c
aziza aminova <aziza@acompleteappliance.com>
2023-11-12 16:08:42 -0500

    56 how towk w/massiv

---
+++ l56.js
@@ -0,0 +1,21 @@
+const myArray = [1, true, 'a']
+console.log(myArray)
+
+console.log(myArray[0])
+console.log(myArray[1])
+console.log(myArray.length)
+
+
+
+const myArray2 = [1,2,3,4]
+console.log(myArray2)
+console.log(myArray2.length)
+
+myArray2[2]='abc'
+
+console.log(myArray2)
+console.log(myArray2[2])
+
+myArray2[4]=true
+console.log(myArray2)
+console.log(myArray2.length)
