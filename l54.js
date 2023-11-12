a328e37c7b7c6d55337440f66453db4e9cc24eaa
aziza aminova <aziza@acompleteappliance.com>
2023-11-12 15:46:42 -0500

    54sravnenie massiv

---
+++ l54.js
@@ -0,0 +1,16 @@
+st myArray2 = [1, true, 'aziza']
+undefined
+myArray2
+(3) [1, true, 'aziza']
+myArray===myArray2
+false
+const myArray3=my
+VM373:1 Uncaught ReferenceError: my is not defined
+at <anonymous>:1:16
+(anonymous) @ VM373:1
+const myArray3=myArray
+undefined
+my
+VM450:1 Uncaught ReferenceError: my is not defined
+at <anonymous>:1:1
+(anonymous) @ VM450:1
