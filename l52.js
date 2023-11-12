92510a31d029a1445f658232053e309fbc6b37a7
aziza aminova <aziza@acompleteappliance.com>
2023-11-12 14:23:59 -0500

    2 raznica instr vyirazh

---
+++ l52.js
@@ -0,0 +1,14 @@
+function myFn(a) {
+    console.log(a);
+}
+
+const b=true;
+let c=10;
+
+console.log(myFn(2+3))
+console.log(myFn(b))
+console.log(myFn(c=c+1))
+
+console.log(myFn(c=c+1;))
+console.log(myFn(let d))
+
