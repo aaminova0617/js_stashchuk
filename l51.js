f644044339de8d164bbb50281f4976b28935779a
aziza aminova <aziza@acompleteappliance.com>
2023-11-12 14:14:15 -0500

    51 vyirazh-instrukc

---
+++ l51.js
@@ -0,0 +1,16 @@
+'abc'
+
+a=a+3;
+c=a+b;
+d='Good'+'Evening';
+
+myFunction(c,d)
+console.log('Hey')
+
+if (a>b) {
+    console.log('a is larger')
+}
+
+for (let i=0 i++ i<5) {
+    console.log(i)
+}
