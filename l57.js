c3c83c44655d2be243e048bcfc89095a7f1c1b54
aziza aminova <aziza@acompleteappliance.com>
2023-11-12 16:30:51 -0500

    57 methods of massiv

---
+++ l57.js
@@ -0,0 +1,46 @@
+/*
+const myArray = [1,2,3]
+console.log(myArray)
+
+myArray.push(5)
+
+console.log(myArray)
+
+myArray.push(true)
+console.log(myArray)
+
+
+const myArray1=[1,2,3]
+console.log(myArray1)
+
+myArray1.pop()
+console.log(myArray1)
+
+const removedElement = myArray1.pop ()
+
+console.log(myArray1)
+console.log(removedElement)
+
+*/
+
+/*
+const myArray2 = [1,2,3]
+console.log(myArray2)
+
+myArray2.unshift(true)
+console.log(myArray2)
+
+myArray2.unshift('abc')
+console.log(myArray2)
+*/
+
+const myArray3 = [1,2,3]
+console.log(myArray3)
+
+myArray3.shift()
+console.log(myArray3)
+
+const removedElement = myArray3.shift()
+console.log(myArray3)
+console.log(removedElement)
+
