3c86eddbcf84ea7515108d005e60aa0893213ffc
aziza aminova <aziza@acompleteappliance.com>
2023-11-13 18:50:21 -0500

    60 destruturizacia

---
+++ l60.js
@@ -0,0 +1,13 @@
+const userProfile = {
+    name: 'Bogdan',
+    commentsQty: 23,
+    hasSignedAgreement: false,
+}
+
+// const {name, commentsQty} = userProfile
+// const {hasSignedAgreement} = userProfile
+const {name, commentsQty, hasSignedAgreement} = userProfile
+
+console.log(name)
+console.log(commentsQty)
+console.log(hasSignedAgreement)
